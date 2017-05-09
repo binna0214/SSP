f7e6e19aa25da8665cd79d41087b4ed18af92981
binna0214 <aumi0214@hanmail.net>
2017-05-10 03:48:22 +0900

    popen.c
    popen

---
+++ popen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAXLINE 256
+
+int main()
+{
+    FILE *fp;
+    int state;
+
+    char buff[MAXLINE];
+    fp = popen("ls -al", "r");
+    if (fp == NULL)
+    {
+        perror("erro : ");
+        exit(0);
+    }
+
+    while(fgets(buff, MAXLINE, fp) != NULL)
+    {
+        printf("%s", buff);
+    }
+
+    state = pclose(fp);
+    printf("state is %d\n", state);
+}
